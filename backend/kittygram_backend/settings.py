4d8558ad56ee2a579492257e0e129aae092ebc58
BKSLab <bks2408@mail.ru>
2023-08-03 21:51:54 +0400

    deploy kittygram_project after review. Version: 01

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'data_is_hidden_from_you')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
